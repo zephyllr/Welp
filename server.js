2d8ea7d4a428734a22c3133321b71e4014798606
whc105 <whc309@nyu.edu>
2018-09-29 16:06:19 -0400

    Backend setup

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ app.use(express.static(path.join(__dirname, 'dist/welp')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//require('./db');
+const getYelpLocations = require('./api/getYelpLocations');
+
+getYelpLocations(app);
+
+require('./db');
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/welp/index.html'));
